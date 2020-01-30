ae994395474144a6e170e6b19220a8e466cada85
Wolfgang Ebner <wolfgang.p.ebner@gmail.com>
2020-01-30 14:31:22 +0100

    Update openshift_urls.py

--- openshift/netbox/openshift_urls.py
+++ openshift/netbox/openshift_urls.py
@@ -5,8 +5,8 @@ Override the Netbox urls.py for OAuth login
 from django.urls import path
 from netbox.openshift_auth import LogoutView
 
-urlpatterns = [
+_patterns = [
     path(r'logout/', LogoutView.as_view(), name='logout'),
-] + urlpatterns + [
+] + _patterns + [
     path(r'oauth/', include('social_django.urls', namespace='social')),
 ]
