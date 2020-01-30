92c884bc49ac207315a712bf351afc26e97d7ae2
Wolfgang Ebner <wolfgang.p.ebner@gmail.com>
2020-01-30 14:28:14 +0100

    Update openshift_urls.py
    urls: fix 3168 by changing url to path

--- openshift/netbox/openshift_urls.py
+++ openshift/netbox/openshift_urls.py
@@ -2,11 +2,11 @@
 Override the Netbox urls.py for OAuth login
 """
 
-from netbox.urls import *
+from django.urls import path
 from netbox.openshift_auth import LogoutView
 
 urlpatterns = [
-    url(r'^logout/$', LogoutView.as_view(), name='logout'),
+    path(r'logout/', LogoutView.as_view(), name='logout'),
 ] + urlpatterns + [
-    url(r'^oauth/', include('social_django.urls', namespace='social')),
+    path(r'oauth/', include('social_django.urls', namespace='social')),
 ]
